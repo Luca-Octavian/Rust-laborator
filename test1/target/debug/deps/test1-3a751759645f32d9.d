5fefd92f440d3064dad610217b2c1edb6a36d83b
Luca-Octavian <luca03octavian@gmail.com>
2025-11-08 14:15:46 +0200

    cleaned up trash

--- test1/target/debug/deps/test1-3a751759645f32d9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Luca\Documents\probleme rust\test1\target\debug\deps\test1-3a751759645f32d9.d: src\main.rs
-
-C:\Users\Luca\Documents\probleme rust\test1\target\debug\deps\libtest1-3a751759645f32d9.rmeta: src\main.rs
-
-src\main.rs:
--- test1/target/debug/test1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Luca\Documents\probleme\ rust\test1\target\debug\test1.exe: C:\Users\Luca\Documents\probleme\ rust\test1\src\main.rs
