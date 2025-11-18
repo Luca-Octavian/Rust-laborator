1f6cb189e4b4a53fec70adf0bb8647af1254fab6
Luca-Octavian <luca03octavian@gmail.com>
2025-11-18 14:00:55 +0200

    removed junk

--- test1/target/debug/deps/test1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Luca\Documents\probleme rust\test1\target\debug\deps\test1.d: src\main.rs
-
-C:\Users\Luca\Documents\probleme rust\test1\target\debug\deps\test1.exe: src\main.rs
-
-src\main.rs:
